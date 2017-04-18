583069d11b4b2db300968f1c7929db2394e9d291
Henrik Bjelke Anderson <henriban@stud.ntnu.no>
2017-04-18 16:47:16 +0200

    Added sort to table

--- src/components/mainComponent.js
+++ src/components/mainComponent.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Search from './searchComponent';
-//import Sortabel from './sortabelComponent';
 import Result from './resultComponent';
 import Table from './tableComponent';
 
@@ -52,16 +51,6 @@ class Main extends React.Component {
             .filter(x => x.occupation.toLowerCase().includes(searchList.occupation.toLowerCase()))
             .filter(x => x.parents_background.toLowerCase().includes(searchList.parents_background.toLowerCase()))
         });
-        // this.setState({filteredList: this.state.rows
-        //     .filter(x => x.place.toLowerCase().includes(searchList.place))
-        //     .filter(x => x.gender.toLowerCase().includes(searchList.gender))
-        //     .filter(x => x.age.toLowerCase().includes(searchList.age))
-        //     .filter(x => x.birth.toLowerCase().includes(searchList.birth))
-        //     .filter(x => x.date_of_recording.toLowerCase().includes(searchList.date_of_recording))
-        //     .filter(x => x.education.toLowerCase().includes(searchList.education))
-        //     .filter(x => x.occupation.toLowerCase().includes(searchList.occupation))
-        //     .filter(x => x.parents_background.toLowerCase().includes(searchList.parents_background))
-        // });
     }
 
     onSearchChange(list){
@@ -79,7 +68,6 @@ class Main extends React.Component {
                 {this.state.showResult && <Result onCloseClick={this.onShowResultChange} inf={this.state.resultId}/>}
                 <div className="main">
                     <Search onSearchChange={this.onSearchChange} />
-                    {/*<Sortabel onRowClick={this.onShowResultChange}/>*/}
                     <Table rows={this.state.filteredList} onRowClick={this.onShowResultChange} searchList={this.state.searchList} />
                 </div>
             </div>
